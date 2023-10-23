f809b2d8c60917885a46c42431824887b1ba1320
V-SLOPE1 <140408735+V-SLOPE1@users.noreply.github.com>
2023-10-23 05:13:33 -0700

    Add files via upload

---
+++ LearnerSystem/src/main/java/com/mycompany/learnersystem/LearnerSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.learnersystem;
+
+/**
+ *
+ * @author intern
+ */
+public class LearnerSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
